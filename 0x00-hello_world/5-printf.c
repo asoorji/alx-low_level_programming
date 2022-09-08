da64b083922d8f712069daea6eff9477fb3b390c
Aso Orji <42071241+asoorji@users.noreply.github.com>
2022-09-08 13:47:50 +0100

    Add files via upload

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - Prints "with proper grammar, but the outcome is a piece of art,\n"
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
