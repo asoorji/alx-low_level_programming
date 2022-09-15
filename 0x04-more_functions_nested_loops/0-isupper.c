017c00a916735d4ad450d40efe89c4316043e5e2
Aso Orji <42071241+asoorji@users.noreply.github.com>
2022-09-15 15:49:34 +0200

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,13 @@
 
 /**
  * _isupper - checks if the input character is uppercase
- * @c: The ASCII equivalent of the character
- * Return: 1 if the true 0 if false
- * /
- *
- * int _isupper(int c)
- * {
- *	if(c>='A' && c<='Z')
- *	    return (1);
- *	else
- *	    return (0);
- * }
+ * @c: the ASCII equivalent of the character
+ * Return: 1 if true 0 if false
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
