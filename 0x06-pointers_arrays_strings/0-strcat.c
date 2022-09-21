82327d6376b78e712ae5478dff2f0b18fb2f8ba0
Aso Orji <ndubuisiaso@gmail.com>
2022-09-21 10:22:57 -0700

    o-strcat file added

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h
+#include <stdio.h>
+
+/**
+*_strcat - concatenates two strings
+*dest - first string
+*src - second string
+* Return: void.
+*/
+char *_strcat(char *dest, char *src)
+{
+	char *dest = *dest + *src;
+	return *dest;
+}
+
+
