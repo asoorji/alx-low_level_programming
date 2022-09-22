d3ca2b6cd3af00725b14a024bd8f90a26eb80df4
Aso Orji <42071241+asoorji@users.noreply.github.com>
2022-09-22 08:12:17 +0200

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,53 +1,47 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * infinite_add - add two numbers
- * @n1: num1 param.
- * @n2: num2 param.
- * @r: buffer that the function will use to store the result
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer for result
  * @size_r: buffer size
- * Return: char*
+ *
+ * Return: address of r or 0
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int i = 0, j = 0, calc, longest, n1int, n2int, sum = 0;
-while (n1[i] != '\0')
-i++;
-while (n2[j] != '\0')
-j++;
-if (i >= j)
-longest = i;
-else
-longest = j;
-if ((longest + 1) >= size_r)
-return (0);
-r[longest + 1] = '\0';
-i--, j--, size_r--;
-n1int = n1[i] - 48, n2int = n2[j] - 48;
-while (longest >= 0)
-{
-calc = n1int + n2int + sum;
-if (calc >= 10)
-sum = calc / 10;
-else
-sum = 0;
-if (calc > 0)
-r[longest] = (calc % 10) + 48;
-else
-r[longest] = '0';
-if (i > 0)
-i--, n1int = n1[i] - 48;
-else
-n1int = 0;
-if (j > 0)
-j--, n2int = n2[j] - 48;
-else
-n2int = 0;
-longest--, size_r--;
-}
-if (r[0] == '0')
-return (r + 1);
-else
-return (r);
+	int i, j, k, l, m, n;
+
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
+		return (0);
+	m = 0;
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+	{
+		n = m;
+		if (i >= 0)
+			n += n1[i] - '0';
+		if (j >= 0)
+			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
+		{
+			break;
+		}
+		m = n / 10;
+		r[k] = n % 10 + '0';
+	}
+	r[k] = '\0';
+	if (i >= 0 || j >= 0 || m)
+		return (0);
+	for (k -= 1, l = 0; l < k; k--, l++)
+	{
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
+	}
+	return (r);
 }
