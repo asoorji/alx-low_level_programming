6e559be7a79cdfe428ea969b3dac4ac7c8838c31
Aso Orji <ndubuisiaso@gmail.com>
2022-10-04 10:46:12 -0700

    Create array file added

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * create_array - a function to create arrays
+ *
+ *   * @size: the size of the proposed array
+ *
+ *    * @c: the default values of the proposed array
+ *
+ *     * Return: null or the pointer to the array
+ *
+ *      */
+
+
+
+char *create_array(unsigned int size, char c)
+
+{
+
+	unsigned int count;
+
+	char *reserved_space;
+
+
+
+	if (size == 0)
+
+		return (NULL);
+
+
+
+	reserved_space = (char *)malloc(size * sizeof(char));
+
+
+
+	if (reserved_space == NULL)
+
+		return (NULL);
+
+
+
+	for (count = 0; count < size; count++)
+
+		reserved_space[count] = c;
+
+
+
+	return (reserved_space);
+
+}
+
+
