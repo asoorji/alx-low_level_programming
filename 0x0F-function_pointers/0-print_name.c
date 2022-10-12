4fa2ce9037e6c7974d4dce231a3dda9122072c87
Aso Orji <ndubuisiaso@gmail.com>
2022-10-12 06:33:51 -0700

    function pointer scripts

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,27 @@
+nclude "function_pointers.h"
+
+
+
+/**
+ *
+ *  * print_name - function that prints a name
+ *
+ *   * @name: the input name to print
+ *
+ *    * @f: the function to use to print
+ *
+ *     * Return: void
+ *
+ *      */
+
+void print_name(char *name, void (*f)(char *))
+
+{
+
+	    if (name != NULL && f != NULL)
+
+			    f(name);
+
+}
+
+
