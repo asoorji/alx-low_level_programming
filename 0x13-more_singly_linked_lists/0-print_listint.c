ba2dce900e781de02f7c1fb63ffbea1caf6d3bf5
Aso Orji <ndubuisiaso@gmail.com>
2022-10-24 14:12:35 -0700

    alx more linkedlist

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * print_listint - prints all the items in a SLL
+ *
+ *   * @h: pointer to the head
+ *
+ *    *
+ *
+ *     * Return: numbers of nodes
+ *
+ *      */
+
+size_t print_listint(const listint_t *h)
+
+{
+
+	size_t count = 0;
+
+
+
+	while (h != NULL)
+
+	{
+
+		printf("%d\n", h->n);
+
+		h = h->next;
+
+		count++;
+
+	}
+
+	return (count);
+
+}
+
+
